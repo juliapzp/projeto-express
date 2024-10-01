b6b993b153799fcbbf2e87744ed29018c82ac094
JÚLIA PICKCIUS ZANIN PEREIRA <julia_p_pereira@estudante.sesisenai.org.br>
2024-10-01 11:52:43 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
